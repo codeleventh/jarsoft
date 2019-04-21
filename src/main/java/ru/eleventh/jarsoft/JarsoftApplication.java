e42c1034f2a556a0715ce7db8a53a8ee6f9e3dc4
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2019-04-21 20:26:33 +0700

    init()

---
+++ src/main/java/ru/eleventh/jarsoft/JarsoftApplication.java
@@ -0,0 +1,13 @@
+package ru.eleventh.jarsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarsoftApplication.class, args);
+	}
+
+}
